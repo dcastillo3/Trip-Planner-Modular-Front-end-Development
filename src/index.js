be4d7fa6ca150141ff978320d1ba6567cd301fbd
Daniel Castillo <dcastillo3@gmail.com>
2018-05-04 12:04:36 -0400

    devtool quickfix for mapbox

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+//This is Brooklyn
 console.log("Hello from Brooklyn");
+
+//This is our Mapbox
+const mapboxgl = require("mapbox-gl");
+
+mapboxgl.accessToken = "pk.eyJ1IjoiZGNhc3RpbGxvMyIsImEiOiJjamdzM3ZlMWkwZDc4MnFtcTJsdWhseWJjIn0.hN92Yme2y69jkMdTCASk5w";
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
